a944a7bc9777622e0907f916fe5f8c55662cd1fc
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-19 15:31:15 +0800

    Save

--- MNIST_Fashion.py
+++ MNIST_Fashion.py
@@ -56,11 +56,14 @@ def app():
     learning algorithms, particularly those suited for image classification. 
     \nRelatively small size: With 70,000 images, it's computationally efficient to train and 
     test on, making it ideal for initial experimentation and algorithm evaluation.
-    \nSimple image format: The images are grayscale and low-resolution (28x28 pixels), 
+    \nSimple image format: 
+    The images are grayscale and low-resolution (28x28 pixels), 
     simplifying preprocessing and reducing computational demands.
-    \nMultiple classes: It consists of 10 distinct clothing categories, allowing you to assess 
+    \nMultiple classes: 
+    It consists of 10 distinct clothing categories, allowing you to assess 
     the classifiers' ability to differentiate between various categories.
-    \nBenchmarking: As a widely used dataset, it facilitates comparison of your models' 
+    \nBenchmarking: 
+    As a widely used dataset, it facilitates comparison of your models' 
     performance with established benchmarks for these algorithms on the same dataset."""
     st.write(text)
 
