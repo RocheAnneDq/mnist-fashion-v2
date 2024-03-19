c65c7f1245ddcc2ec81f3acbd6cec6fbcf713286
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-19 15:37:46 +0800

    Save

--- pages/2Performance.py
+++ pages/2Performance.py
@@ -34,37 +34,38 @@ def app():
         clf = KNeighborsClassifier(n_neighbors=5)
         selected_model = 0
 
-    classifier = ''
-    if selected_model == 0:    
-        report = """K-Nearest Neighbor"""
-        classifier = 'K-Nearest Neighbor'
-    elif selected_model == 1:   # Random Forest
-        report = """Support Vector Mach8ine"""
-        classifier = 'Support Vector Machine'
-    else:        
-        report = """Naive Bayes"""
-        classifier = "Naive Bayes"
+    if st.button("Begin Test"):
+        classifier = ''
+        if selected_model == 0:    
+            report = """K-Nearest Neighbor"""
+            classifier = 'K-Nearest Neighbor'
+        elif selected_model == 1:   # Random Forest
+            report = """Support Vector Mach8ine"""
+            classifier = 'Support Vector Machine'
+        else:        
+            report = """Naive Bayes"""
+            classifier = "Naive Bayes"
 
-    st.subheader('Performance of the ' + classifier)
+        st.subheader('Performance of the ' + classifier)
 
-    X_train = st.session_state['X_train']
-    X_test = st.session_state['X_test']
-    y_train= st.session_state['y_train']
-    y_test = st.session_state['y_test']
+        X_train = st.session_state['X_train']
+        X_test = st.session_state['X_test']
+        y_train= st.session_state['y_train']
+        y_test = st.session_state['y_test']
 
-    clf.fit(X_train, y_train)
-    y_test_pred = clf.predict(X_test)
+        clf.fit(X_train, y_train)
+        y_test_pred = clf.predict(X_test)
 
-    st.subheader('Confusion Matrix')
-    st.write('Confusion Matrix')
-    cm = confusion_matrix(y_test, y_test_pred)
-    st.text(cm)
+        st.subheader('Confusion Matrix')
+        st.write('Confusion Matrix')
+        cm = confusion_matrix(y_test, y_test_pred)
+        st.text(cm)
 
-    st.subheader('Performance Metrics')
-    st.text(classification_report(y_test, y_test_pred))
-    
-    st.write(classifier)
-    st.write(report)
+        st.subheader('Performance Metrics')
+        st.text(classification_report(y_test, y_test_pred))
+        
+        st.write(classifier)
+        st.write(report)
 
 
 #run the app
