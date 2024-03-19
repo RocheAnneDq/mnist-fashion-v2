4e38492f039b2bf7d6800f793e53779ac83f2ce0
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-19 15:12:11 +0800

    Save

--- pages/2Performance.py
+++ pages/2Performance.py
@@ -17,53 +17,30 @@ def app():
     st.sidebar.subheader('Select the classifier')
     # Create the selecton of classifier
     clf = KNeighborsClassifier(n_neighbors=5)
+    selected_model = 0
+
     options = ['K Nearest Neighbor', 'Support Vector Machine', 'Naive Bayes']
     selected_option = st.sidebar.selectbox('Select the classifier', options)
     if selected_option =='Support Vector Machine':
         clf = SVC(kernel='linear')
-        st.session_state['selected_model'] = 1
+        selected_model = 1
     elif selected_option=='Naive Bayes':        
         clf = GaussianNB()
-        st.session_state['selected_model'] = 2
+        selected_model = 2
     else:
         clf = KNeighborsClassifier(n_neighbors=5)
-        st.session_state['selected_model'] = 0
-
+        selected)model = 0
 
     classifier = ''
-    if st.session_state['selected_model'] == 0:     # decision tree
-        report = """Achieves good accuracy, but can be prone to 
-        overfitting, leading to lower performance on unseen data.
-        Simple and interpretable, allowing visualization of decision rules.
-        Susceptible to changes in the training data, potentially 
-        leading to high variance in predictions."""
-        classifier = 'Decision Tree'
-    elif st.session_state['selected_model'] == 1:   # Random Forest
-        report = """Generally outperforms a single decision tree, 
-        reaching accuracy close to 98%. Reduces overfitting through 
-        averaging predictions from multiple trees. Ensemble method - 
-        combines predictions from multiple decision trees, leading to 
-        improved generalization and reduced variance. Less interpretable 
-        compared to a single decision tree due to the complex 
-        ensemble structure."""
-        classifier = 'Random Forest'
-    elif st.session_state['selected_model'] == 2:   # Extreme Random Forest
-        report = """Performance: Can achieve similar or slightly better 
-        accuracy compared to a random forest, but results can vary 
-        depending on hyperparameter tuning. Introduces additional randomness 
-        during tree building by randomly selecting features at each split.  Aims to 
-        further improve generalization and reduce overfitting by increasing 
-        the diversity of trees in the ensemble. Requires careful 
-        hyperparameter tuning to achieve optimal performance."""
-        classifier = "Extreme Random Forest"
-    else:
-        report = """Accuracy: While KNN can achieve reasonable accuracy (around 80-90%), 
-        it's often outperformed by more sophisticated models like Support 
-        Vector Machines (SVMs) or Convolutional Neural Networks (CNNs) which can 
-        reach over 97% accuracy.\nComputational cost: Classifying new data points 
-        requires comparing them to all data points in the training set, making 
-        it computationally expensive for large datasets like MNIST."""
-        classifier = "K-Nearest Neighbor"
+    if selected_model == 0:    
+        report = """K-Nearest Neighbor"""
+        classifier = 'K-Nearest Neighbor'
+    elif selected_model == 1:   # Random Forest
+        report = """Support Vector Mach8ine"""
+        classifier = 'Support Vector Machine'
+    else:        
+        report = """Naive Bayes"""
+        classifier = "Naive Bayes"
 
     st.subheader('Performance of the ' + classifier)
 
@@ -87,8 +64,6 @@ def app():
     st.write(classifier)
     st.write(report)
 
-    # save the clf to the session state
-    st.session_state['clf'] = clf
 
 #run the app
 if __name__ == "__main__":
