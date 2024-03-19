97c57f20e9d54c2d2358ebd960982ba3d49635a8
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-19 14:45:11 +0800

    Save

--- MNIST_Fashion.py
+++ MNIST_Fashion.py
@@ -16,11 +16,7 @@ import time
 
 # Define the Streamlit app
 def app():
-
-    if "reset_app" not in st.session_state:
-        st.session_state.reset_app = False
-
-    text = """Insert Your App Title Here"""
+    text = """MNIST Fasion Dataset"""
     st.subheader(text)
 
     # Use session state to track the current form
@@ -53,65 +49,22 @@ def app():
     West Visayas State University"""
     st.text(text)
 
-    st.image('MNIST.png', caption="Replace he image and replace this text with the description""")
+    st.image('mnist-fashion.png', caption="MNISt Fashion Dataset""")
 
-    if "dataset_ready" not in st.session_state:
-        # Create a progress bar object
-        progress_bar = st.progress(0, text="Loading the dataset please wait...")
+    text = """Modified National Institute of Standards and Technology", use_column_width=True)
+    The Fashion MNIST dataset is a popular choice for testing and comparing machine
+    learning algorithms, particularly those suited for image classification. 
+    \nRelatively small size: With 70,000 images, it's computationally efficient to train and 
+    test on, making it ideal for initial experimentation and algorithm evaluation.
+    \nSimple image format: The images are grayscale and low-resolution (28x28 pixels), 
+    simplifying preprocessing and reducing computational demands.
+    \nMultiple classes: It consists of 10 distinct clothing categories, allowing you to assess 
+    the classifiers' ability to differentiate between various categories.
+    \nBenchmarking: As a widely used dataset, it facilitates comparison of your models' 
+    performance with established benchmarks for these algorithms on the same dataset."""
+    st.write(text)
 
-        # replace with your dataset
-        # Load MNIST dataset
-        #st.session_state.mnist = fetch_openml('mnist_784', version=1, data_home=".", return_X_y=True)
-
-        for i in range(100):
-            # Update progress bar value
-            progress_bar.progress(i + 1)
-            # Simulate some time-consuming task (e.g., sleep)
-            time.sleep(0.01)
-
-        # Progress bar reaches 100% after the loop completes
-        st.success("Dataset loading completed!")
-        st.session_state.dataset_ready = True
-
- 
-    """
-    # Extract only the specified number of images and labels
-    size = 10000
-    X, y = st.session_state.mnist
-    X = X[:size]
-    y = y[:size]
-
-    # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    #save the values to the session state    
-    st.session_state['X_train'] = X_train
-    st.session_state['X_test'] = X_test
-    st.session_state['y_train'] = y_train
-    st.session_state['y_test'] = y_test    
-    """
-
-    st.sidebar.subheader('Select the classifier')
-
-    # Create the selection of classifier
-    clf = DecisionTreeClassifier()
-    options = ['Decision Tree', 'Random Forest Classifier', 'Extreme Random Forest Classifier', 'K-Nearest Neighbor']
-    selected_option = st.sidebar.selectbox('Select the classifier', options)
-    if selected_option =='Random Forest Classifier':
-        clf = RandomForestClassifier(n_jobs=2, random_state=0)
-        st.session_state['selected_model'] = 1
-    elif selected_option=='Extreme Random Forest Classifier':        
-        clf = ExtraTreesClassifier(n_estimators=100, max_depth=4, random_state=0)
-        st.session_state['selected_model'] = 2
-    elif selected_option == 'K-Nearest Neighbor':
-        clf = KNeighborsClassifier(n_neighbors=5)
-        st.session_state['selected_model'] = 3
-    else:
-        clf = DecisionTreeClassifier()
-        st.session_state['selected_model'] = 0
-
-    # save the clf to the session variable
-    st.session_state['clf'] = clf
+   
     
 #run the app
 if __name__ == "__main__":
